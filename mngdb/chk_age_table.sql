197ab9211ca88018396014690c942bad567f8958
gpdbkr <59007642+gpdbkr@users.noreply.github.com>
2022-04-07 08:53:56 +0900

    Create chk_age_table.sql

---
+++ mngdb/chk_age_table.sql
@@ -0,0 +1,13 @@
+SELECT
+    c.gp_segment_id,
+    coalesce(n.nspname, ‘’) as nspname,
+    c.relname,
+    c.relkind,
+    c.relstorage,
+    age(c.relfrozenxid) as age
+FROM
+    gp_dist_random(‘pg_class’) c
+    LEFT JOIN pg_namespace n ON c.relnamespace = n.oid
+WHERE
+    c.relkind = ‘r’ AND c.relstorage NOT IN (‘x’)
+ORDER BY 6 DESC;
