0c058a0395ab046f3ae96397d4aa9484d5558660
gpdbkr <59007642+gpdbkr@users.noreply.github.com>
2022-04-07 11:26:41 +0900

    Create pgb_user.sh

---
+++ stattool/pgb_user.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+source /home/gpadmin/.bash_profile
+
+i=0
+while [ $i -lt $2 ]
+do
+psql -p 6543 -d pgbouncer -tc "show clients" | grep -v "^$" | awk '{print strftime("%Y-%m-%d %H:%M:%S"), $3;}' | uniq -c | awk '{print $2, $3 "|" $4, "|" $1}'
+sleep 1
+i=`expr $i + 1`
+done
