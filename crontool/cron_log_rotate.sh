cc75fe94af65d03c8c3efc284bb4be5b48f326b7
zhyun-pivotal <52663478+zhyun-pivotal@users.noreply.github.com>
2023-11-20 21:23:52 +0900

    Create cron_log_rotate.sh

---
+++ crontool/cron_log_rotate.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#export MON_HOME="/data/utilities"
+#source ${MON_HOME}/setup/cluster_info.sh
+
+psql -c "select pg_logfile_rotate()"
+
+JOBDATE=`date -d -12hour '+%Y-%m-%d_%H*'`
+FILENAME=gpdb-${JOBDATE}
+DIRCHK=`/usr/bin/find /data/master/gpseg-1/pg_log/pglogbak -type d | wc -l`
+
+if [ ${DIRCHK} -eq 0 ]
+then
+	mkdir -p /data/master/gpseg-1/pg_log/pglogbak
+else
+	echo "pglogbak directory already exists"
+fi
+
+/bin/mv /data/master/gpseg-1/pg_log/${FILENAME}.csv /data/master/gpseg-1/pg_log/pglogbak
+/bin/gzip /data/master/gpseg-1/pg_log/pglogbak/${FILENAME}.csv
+
+/usr/bin/find /data/master/gpseg-1/pg_log/pglogbak/*.csv.gz -mtime +60 -exec rm -f '{}' \;
