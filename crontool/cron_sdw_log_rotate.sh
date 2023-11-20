5f40862c10ee9f5cdea9f0752ee91b9624b9b52c
zhyun-pivotal <52663478+zhyun-pivotal@users.noreply.github.com>
2023-11-20 21:25:54 +0900

    Create cron_sdw_log_rotate.sh

---
+++ crontool/cron_sdw_log_rotate.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+source ~/.bashrc
+
+psql -Atc "select hostname, datadir from gp_segment_configuration where content != -1;" > /data/utilities/tmp_delete.txt
+
+for HOST_DIR in `cat /data/utilities/tmp_delete.txt`
+	do
+	HOSTNAME=`echo $HOST_DIR | awk -F "|" '{print $1}'`
+	DIR=`echo $HOST_DIR | awk -F "|" '{print $2}'`
+
+	ssh ${HOSTNAME} ". ~/.bash_profile;find ${DIR}/pg_log/*.csv* -mtime +60 -exec /bin/rm -f '{}' \;"
+	ssh ${HOSTNAME} ". ~/.bash_profile;find ${DIR}/pg_log/*.csv -mtime +1 -exec /bin/gzip '{}' \;"
+done
+
+/bin/rm -f /data/utilities/tmp_delete.txt
