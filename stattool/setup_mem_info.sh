f06a60c9b991bc980e641ade787742c66d4f4728
zhyun-pivotal <52663478+zhyun-pivotal@users.noreply.github.com>
2023-11-21 09:09:01 +0900

    Update setup_mem_info.sh

--- stattool/setup_mem_info.sh
+++ stattool/setup_mem_info.sh
@@ -5,21 +5,24 @@ SDW_CNT=`cat ${GPKRUTIL}/hostfile_seg | wc -l`
 SMDW_CNT=`ping -c 1 smdw | grep received | awk '{print $4}'`
 
 ### make utilities dir
-echo "Make /home/gpadmin/gpkrutil/stattool to all nodes"
+#echo "Make /home/gpadmin/gpkrutil/stattool to all nodes"
+#
+#if [ $SMDW_CNT -ne 0 ]
+#then
+#	ssh smdw mkdir -p /home/gpadmin/gpkrutil/stattool
+#	ssh smdw chown -R /home/gpadmin/gpkrutil/stattool
+#else
+#	echo "smdw skip make directory"
+#fi
+#
+#for ((i=1;i<=$SDW_CNT;i++))
+#	do
+#	ssh sdw${i} mkdir -p /home/gpadmin/gpkrutil/stattool
+#	ssh sdw${i} chown -R gpadmin:gpadmin /home/gpadmin/gpkrutil/stattool
+#done
+echo "Make /home/gpadmin/memutil/ to all nodes"
 
-if [ $SMDW_CNT -ne 0 ]
-then
-	ssh smdw mkdir -p /home/gpadmin/gpkrutil/stattool
-	ssh smdw chown -R /home/gpadmin/gpkrutil/stattool
-else
-	echo "smdw skip make directory"
-fi
-
-for ((i=1;i<=$SDW_CNT;i++))
-	do
-	ssh sdw${i} mkdir -p /home/gpadmin/gpkrutil/stattool
-	ssh sdw${i} chown -R gpadmin:gpadmin /home/gpadmin/gpkrutil/stattool
-done
+gpssh -f ${GPKRUTIL}/hostfile_all '
 
 ### scp mem_info.sh
 echo "Copy mem_info.sh to all segments"
