262a4425552ef2dedd337ccb29539480ea52113f
zhyun-pivotal <52663478+zhyun-pivotal@users.noreply.github.com>
2023-11-21 09:34:05 +0900

    Update meminfo.sh

--- stattool/meminfo.sh
+++ stattool/meminfo.sh
@@ -8,17 +8,18 @@ then
 	exit
 fi
 
-SEG_CNT=`psql -Atc "select count(distinct(hostname)) from gp_segment_configuration where content != -1;"`
+#SEG_CNT=`psql -Atc "select count(distinct(hostname)) from gp_segment_configuration where content != -1;"`
 SLEEP=20
 TCNT=3
 
 for ((j=0;j<$TCNT;j++))
 	do
-	ssh mdw "${GPKRUTIL}/stattool/mem_info.sh"
- 	ssh smdw "/home/gpadmin/gpkrutil/stattool/mem_info.sh"
-	for ((i=1;i<=$SEG_CNT;i++))
-		do
-		ssh sdw${i} "/home/gpadmin/gpkrutil/stattool/mem_info.sh"
-	done
-		sleep $SLEEP
+#	ssh mdw "${GPKRUTIL}/stattool/mem_info.sh"
+# 	ssh smdw "/home/gpadmin/gpkrutil/stattool/mem_info.sh"
+#	for ((i=1;i<=$SEG_CNT;i++))
+#		do
+#		ssh sdw${i} "/home/gpadmin/gpkrutil/stattool/mem_info.sh"
+#	done
+	gpssh -f ${GPKRUTIL}/hostfile_all 'sh /home/gpadmin/memutil/mem_info.sh'
+	sleep $SLEEP
 done
