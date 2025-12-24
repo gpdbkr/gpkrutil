3c36940b3089c7f30ca01c5195ca144549199e2f
zhyun-pivotal <52663478+zhyun-pivotal@users.noreply.github.com>
2025-12-24 16:46:43 +0900

    Update locks.sh

--- stattool/locks.sh
+++ stattool/locks.sh
@@ -10,7 +10,7 @@ fi
 i=0
 while [ $i -lt $2 ]
 do
-psql -c ""
+psql -c "SELECT pid, mppsessionid, relname, locktype, mode, a.gp_segment_id from pg_locks a, pg_class where relation=oid and relname not like 'pg_%' order by 3;"
 sleep $1
 i=`expr $i + 1`
 #echo $i
