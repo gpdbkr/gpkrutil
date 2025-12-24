a44e49f39da5ab4ad93184be2dab0ebef7fab1a6
zhyun-pivotal <52663478+zhyun-pivotal@users.noreply.github.com>
2025-12-24 16:38:04 +0900

    Update lt.sh

--- stattool/lt.sh
+++ stattool/lt.sh
@@ -10,7 +10,8 @@ fi
 i=0
 while [ $i -lt $2 ]
 do
-psql -c "SELECT distinct to_char(now(), 'yyyymmdd hh24:mi:ss') log_tm, w.locktype, w.relation::regclass AS relation, w.mode, w.pid as waiting_pid, other.pid as running_pid, w.gp_segment_id FROM pg_catalog.pg_locks AS w JOIN pg_catalog.pg_stat_activity AS w_stm ON (w_stm.pid = w.pid) JOIN pg_catalog.pg_locks AS other ON ((w.DATABASE = other.DATABASE AND w.relation = other.relation) OR w.transactionid = other.transactionid) JOIN pg_catalog.pg_stat_activity AS other_stm ON (other_stm.pid = other.pid) WHERE NOT w.granted and w.pid <> other.pid;"
+#psql -c "SELECT distinct to_char(now(), 'yyyymmdd hh24:mi:ss') log_tm, w.locktype, w.relation::regclass AS relation, w.mode, w.pid as waiting_pid, other.pid as running_pid, w.gp_segment_id FROM pg_catalog.pg_locks AS w JOIN pg_catalog.pg_stat_activity AS w_stm ON (w_stm.pid = w.pid) JOIN pg_catalog.pg_locks AS other ON ((w.DATABASE = other.DATABASE AND w.relation = other.relation) OR w.transactionid = other.transactionid) JOIN pg_catalog.pg_stat_activity AS other_stm ON (other_stm.pid = other.pid) WHERE NOT w.granted and w.pid <> other.pid;"
+psql -c "SELECT distinct to_char(now(), 'yyyymmdd hh24:mi:ss') log_tm, w.locktype, w.relation::regclass AS relation, w.mode, w.pid as waiting_pid, w.mppsessionid as waiting_ssid, other.pid as running_pid, other.mppsessionid as running_ssid, w.gp_segment_id FROM pg_catalog.pg_locks AS w JOIN pg_catalog.pg_stat_activity AS w_stm ON (w_stm.pid = w.pid) JOIN pg_catalog.pg_locks AS other ON ((w.DATABASE = other.DATABASE AND w.relation = other.relation) OR w.transactionid = other.transactionid) JOIN pg_catalog.pg_stat_activity AS other_stm ON (other_stm.pid = other.pid) WHERE NOT w.granted and w.pid <> other.pid;"
 sleep $1
 i=`expr $i + 1`
 #echo $i
